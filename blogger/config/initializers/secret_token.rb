726e153fcf389a1c7a59e1d59e386557721d93db
Anil Satya <satya82ster@gmail.com>
2014-10-23 19:34:18 +0530

    finally I get to commit
    finally I get to commit

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'be421e0c5231e02679c00599034c2400a1a1b0f65df49eaf30e1445379f5d95f10ef662fdd2d905c12d673b40689972619311bc898871e2fe96663bf0b61a47f'
